9740c16cea5fd420fd498ef15df7f4d1481e1cc9
AlexanderWestman <superalle96@gmail.com>
2025-03-28 11:15:52 +0100

    Gjorde egen klass

---
+++ java/src/Alex.java
@@ -0,0 +1,15 @@
+public class Alex {
+
+    public void food()
+    {
+        System.out.println("Alex äter");
+    }
+    public void hello()
+    {
+        System.out.println("Alex säger hallåååå");
+    }
+    public void drink()
+    {
+        System.out.println("Kaffe är nice");
+    }
+}
