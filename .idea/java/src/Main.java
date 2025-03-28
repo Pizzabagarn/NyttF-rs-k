5f719c441ca5499c565c056edcab5d921ecd77d6
AlexanderWestman <superalle96@gmail.com>
2025-03-28 10:58:37 +0100

    Gjorde ändring

--- .idea/java/src/Main.java
+++ .idea/java/src/Main.java
@@ -4,10 +4,26 @@ public class Main {
     {
         String name;
         Scanner scanner = new Scanner(System.in);
+        Lowisa l1 = new Lowisa();
 
         System.out.println("Skriv in ditt namn");
         name = scanner.nextLine();
+        name.toLowerCase();
 
-        System.out.println(name);
+        if(name == "lowisa")
+        {
+            l1.hello();
+        }
+
+        if(name == "mena")
+        {
+           
+        }
+
+
+
+
+
+
     }
 }
